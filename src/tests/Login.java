614ec791009316ccee1b1c049208507d75aec617
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-05-05 18:45:40 +0200

    update Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -2,6 +2,6 @@ package tests;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("login class is here");
+        System.out.println("login class is here!");
     }
 }
