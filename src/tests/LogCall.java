94d1fe419d11bc11b793a2fb2472e18e6619f065
Mustafa Cihat YAPICI <57036952+mustafacihat@users.noreply.github.com>
2020-05-06 16:49:46 +0200

    Update LogCall.java

--- src/tests/LogCall.java
+++ src/tests/LogCall.java
@@ -3,6 +3,8 @@ package tests;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("Calling Muhtar .....");
+        
+        //ı made some changes
 
     }
 }
